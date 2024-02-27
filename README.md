a1c54e18ce809e19f264cf793a36b9b36bf2cce3
David-Albarracin <90213684+David-Albarracin@users.noreply.github.com>
2024-02-27 08:59:15 -0500

    refactor: :rocket: add Filter and Search Options

--- README.md
+++ README.md
@@ -1,14 +1,53 @@
 ![css](https://raw.githubusercontent.com/David-Albarracin/README_MATERIALS/main/css-html-js.png)
 
 # Descripción
-Pagina web de algunos heroes de Marvel y DC 
+Pagina web de algunos heroes de Marvel y DC con sus opciones de búsqueda y filtrado 
 
-## Se Implementa
-1. Copia el código HTML en tu documento HTML.
-2. Asegúrate de tener las imágenes de los héroes de Marvel y DC en la ruta correcta y ajusta las rutas de las imágenes en el HTML si es necesario.
-3. Asegúrate de tener un contenedor para héroes de Marvel con el id "marvel" y otro contenedor para héroes de DC con el id "dc" en tu HTML.
-4. Copia el código CSS en tu archivo CSS o en línea en el HTML si prefieres.
-5. Copia el código JavaScript en tu archivo JavaScript o en línea en el HTML si prefieres.
 
 ## Código
-El código HTML proporciona una estructura básica para mostrar los héroes de Marvel y DC en tarjetas, y contiene un diálogo para mostrar detalles de los héroes seleccionados. El código CSS proporciona estilos básicos para las tarjetas y el diálogo. El código JavaScript se encarga de generar dinámicamente las tarjetas de los héroes y mostrar los detalles de un héroe seleccionado en el diálogo.
+
+Empezamos en el `Index.html` donde vamos a encontrar la estructura del proyecto. En el header encontramos el select que nos ayuda a filtrar y el cuadro de texto para realizar las búsquedas. En el `main` encontramos la parte donde se muestran los héroes de Marvel y DC dentro de el encontramos el dialog que nos ayuda a mostrar mas información del personaje.
+
+Aquí hay una descripción de los elementos dentro del `main`:
+
+- **Marvel Section**: Esta sección contiene el título "HEROES DE MARVEL" junto con el logotipo de Marvel y una imagen. Luego, hay un contenedor `<div>` con el `id="marvel"` donde se muestran los héroes de Marvel.
+
+- **DC Section**: Similar a la sección de Marvel, esta sección contiene el título "HEROES DE DC" junto con el logotipo de DC y una imagen. Luego, hay un contenedor `<div>` con el `id="dc"` donde se muestran los héroes de DC.
+
+- **Dialog Element**: Este elemento `<dialog>` Se Muestra mas información del héroe en un dialog para que .
+
+Continuamos con las clases de `style.css` donde encontramos el la primera sección dedicada a el root donde creamos la variables de estilos y removemos algunos estilos por defecto del html luego seguimos con la sección de layout donde encontramos algunas clases comunes que nos ayudan cambiar el display del contenedor a agregar margenes entre otros luego algunas clases reusables para los títulos y continuamos con el header 
+```
+.header {
+    //Nos ayuda a Volver el Contenedor flexible le agrega un alto predefinido y alinea el contenido al centro
+}
+
+.input {
+    // Creamos un estilo de input mas agradable con un padding y un border radius de 25px
+}
+
+.search {
+    //Hacemos el contenedor relativo para y le agregamos un padding en x de 2rem
+}
+
+.search input{
+    //Agregamos el un tamaño de 300px responsivo para el input 
+}
+
+.search label{
+    //Ocultamos el label 
+}
+
+.search button{
+    //Movemos el Botón de buscar para ubicar lo en la parte lateral derecha del input con un border de 100% 
+}
+
+.search button i{
+    //Cambiamos el tamaño del icono
+}
+
+```
+
+Ahora el `main.js` que nos ayuda a cargar dinámica mente los heroes y nos ayuda a controlar los eventos que el usuario realize en
+la pagina para ello primero hacemos referencia a los elementos mencionados anteriormente en el html y los guardamos en unas constantes 
+el primer paso es pedir los datos para ello ya contamos con los datos localmente hacemos referencia a la variable heroes
--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-const marvelContainer = document.querySelector("#marvel");
-const dcContainer = document.querySelector("#dc");
-
-const dialog = document.querySelector("dialog");
-
 const heroes = [
     {
         id: "1",
@@ -132,32 +127,83 @@ const heroes = [
 
 ];
 
+/**
+ * 
+ */
 
 
-heroes.forEach(hero => {
-    const card = `
-        <div class="card ${hero.category}">
-            <div class="card-header">
-                <img src="${hero.img}" alt="img">
+const marvelContainer = document.querySelector("#marvel");
+const dcContainer = document.querySelector("#dc");
+
+const selectCategory = document.querySelector('#selectCategory');
+const searchBtn = document.querySelector('#searchBtn')
+const inputSearch = document.querySelector('#searchInput')
+
+const dialog = document.querySelector("dialog");
+
+
+/**
+ * 
+ * @param {Array} heroes 
+ * @param {String} category
+ * Filtrar y Mostrar Heroes  
+ */
+
+const loadData = (heroes) => {
+    //Reiniciar el Contenedor
+    marvelContainer.innerHTML = ``
+    dcContainer.innerHTML = ``
+
+    //Recorrer los Heroes e ir Agregándolos Según Corresponda 
+    heroes.forEach(hero => {
+        const card = `
+            <div class="card ${hero.category}">
+                <div class="card-header">
+                    <img src="${hero.img}" alt="${hero.name}">
+                </div>
+                <div class="card-body">
+                    <h2>${(hero.name).replace("-", " ")}</h2>
+                    <button onclick="showHero(${hero.id})" class="button">
+                        Ver 
+                    </button>
+                </div>
             </div>
-            <div class="card-body">
-                <h2>${(hero.name).replace("-", " ")}</h2>
-                <button onclick="showHero(${hero.id})" class="button">
-                    Ver 
-                </button>
-            </div>
-        </div>
-    `;
-    if (hero.category == "marvel") {
-        marvelContainer.innerHTML += card;
-    }else{
-        dcContainer.innerHTML += card;
-    }
-})
+        `;
+        hero.category == "marvel"? marvelContainer.innerHTML += card : dcContainer.innerHTML += card;
+    })
+
+    //Cambio del Titulo
+    loadTitles()
+}
+
+//Cargamos Los Heroes
+loadData(heroes);
 
 
+/**
+ * Verifica y Modifica el Estado del Titulo
+ */
+function loadTitles() {
+
+    dcContainer.firstChild? 
+        document.querySelector('#dcTitle').classList.remove("d-none") : 
+        document.querySelector('#dcTitle').classList.add("d-none");
+
+    marvelContainer.firstChild? 
+        document.querySelector('#marvelTitle').classList.remove("d-none") : 
+        document.querySelector('#marvelTitle').classList.add("d-none");
+}
+
+/**
+ * 
+ * @param {string} id 
+ * Nos Permite Buscar y Mostrar el Model
+ * showDialog() Propiedad de la etiqueta <dialog>
+ */
 const showHero = (id) =>{
     const hero = heroes.find(hero => hero.id == id)
+
+    //quemas onclick="dialog.close()" para cerrar el dialogo cuando sea necesario 
     dialog.innerHTML = `
         <div class="dialog-buttons">
             <button onclick="dialog.close()" class="button">
@@ -172,8 +218,8 @@ const showHero = (id) =>{
                 <img src="${hero.img}" alt="img">
             </div>
             <div class="dialog-body">
-                <h2 class="text-color">${(hero.name).replace("-", " ")}</h2>
-                <p class="text-color">${hero.description}</p>
+                <h2>${(hero.name).replace("-", " ")}</h2>
+                <p>${hero.description}</p>
                 <span class="text-color-red">${hero.created_at}</span>
             </div>
         </div>
@@ -182,3 +228,64 @@ const showHero = (id) =>{
 }
 
 
+selectCategory.addEventListener('change', (e) =>{
+    //Tomamos el Valor que Cambio
+    const categorySelect = e.target.value;
+
+    //Filtramos Los Heroes
+    const heroesFilter =  heroes.filter(({category}) => category == categorySelect)
+
+    //Comprobamos si son todos de lo Contrario Cargamos de Nuevo todos los heroes 
+    categorySelect == "all"? loadData(heroes) : loadData(heroesFilter)
+})
+
+/**
+ * Función Para Buscar Héroe y Insertarlo en pantalla
+ * @returns En Caso de No Encontrar Héroe Retorna una Alerta
+ */
+const searchHero = () => {
+    //Reiniciamos los Contenedores
+    marvelContainer.innerHTML = ``
+    dcContainer.innerHTML = ``
+
+    //Tomamos el Valor que Exista en el Input de Search
+    const text = inputSearch.value;
+    //Buscamos el Héroe en heroes con un includes para tener mas rango
+    const hero = heroes.find(hero => hero.name.includes(text) ? hero : null);
+
+    //Verificamos el hero
+    if(!hero || text == ""){
+        loadData(heroes)
+        inputSearch.value = ""
+        return alert("No Se Encontraron Resultados")
+    }
+
+    const card =  `
+    <div class="">
+        
+        <div class="">
+            <img src="${hero.img}" alt="img">
+        </div>
+        <div class="">
+            <h2 class="text-color">${(hero.name).replace("-", " ")}</h2>
+            <p class="text-color">${hero.description}</p>
+            <span class="text-color-red">${hero.created_at}</span>
+        </div>
+        <div class="d-flex justify-content-between my-2">
+            <button onclick="(loadData(heroes), inputSearch.value = '')" class="button">
+                <i class='bx bx-arrow-back'></i>
+            </button>
+            <button onclick="(loadData(heroes), inputSearch.value = '')" class="button">
+                <i class='bx bx-x'></i>
+            </button>
+        </div>
+    </div>
+    `
+
+    hero.category == "marvel"? marvelContainer.innerHTML = card : dcContainer.innerHTML = card;
+    loadTitles()
+}
+
+//Eventos Para Search 
+inputSearch.addEventListener('keypress', (e) => e.key == "Enter"? searchHero() : "")
+searchBtn.addEventListener('click', (e) => searchHero())
